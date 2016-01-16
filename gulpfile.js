e2ac08ef6570f41d9ad12aea8ca9babdd59f75f0
geoom <hi.geoom@gmail.com>
2016-01-16 16:28:10 -0500

    add bower tool, add bower config file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var gulp 	= 	require('gulp'),
+var gulp  =   require('gulp'),
 
-	sass 	= 	require('gulp-sass'),
-	minifyCSS = require('gulp-minify-css'),
-	autoprefixer = require('gulp-autoprefixer'),
+  sass  =   require('gulp-sass'),
+  minifyCSS = require('gulp-minify-css'),
+  autoprefixer = require('gulp-autoprefixer'),
 
-	rename = require('gulp-rename'),
-  	connect = 	require('gulp-connect'),
-  	open 	= 	require('gulp-open'),
-  	concat 	=  	require('gulp-concat'),
-  	port = process.env.port || 3031;
+  rename = require('gulp-rename'),
+    connect =   require('gulp-connect'),
+    open  =   require('gulp-open'),
+    concat  =   require('gulp-concat'),
+    port = process.env.port || 3031;
 
 require('es6-promise').polyfill(); // polyfill the Promise API to support node 0.10
 
@@ -44,11 +44,11 @@ gulp.task('sass', function () {
 // live reload css
 gulp.task('css', function(){
   gulp.src('./assets/css/*.css')
-  	.pipe(minifyCSS())
-  	.pipe(rename('styles.min.css'))
-  	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
-  	.pipe(concat('styles.min.css'))
-  	.pipe(gulp.dest('./assets/css'))
+    .pipe(minifyCSS())
+    .pipe(rename('styles.min.css'))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
+    .pipe(concat('styles.min.css'))
+    .pipe(gulp.dest('./assets/css'))
     .pipe(connect.reload());
 });
 
@@ -62,16 +62,16 @@ gulp.task('html', function(){
 
 // watch files for live reload
 gulp.task('watch', function(){
-	gulp.watch('./assets/scss/**/*.scss', ['sass']);
-  	gulp.watch('./assets/css/*.css', ['css']);
-  	gulp.watch('./index.html', ['html']);
+  gulp.watch('./assets/scss/**/*.scss', ['sass']);
+    // gulp.watch('./assets/css/*.css', ['css']);
+    gulp.watch('./index.html', ['html']);
 });
 
 
 // Gulp named tasks
 gulp.task('default', ['open']);
 gulp.task('build', ['sass', 'css']);
-gulp.task('serve', ['connect', 'open', 'build', 'watch']);
+gulp.task('serve', ['connect', 'open', 'watch']);
 
 
 
